765308939ec6cf0fecc86be1ba640590cf92a9b4
Vinnie Moscaritolo <vinnie@vmeng.com>
2022-04-16 10:14:16 -0700

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -78,8 +78,8 @@ int main(int argc, const char * argv[]) {
 			if(!vfd.begin()) Exception("failed to reset VFD "); continue; }
 
 	 
-		const uint8_t* msg = (uint8_t*) "\x1B\x00\x24\x10\x15\x1FPLEASE WAIT\x10\x1E\29INITIALIZNG SYSTEM";
-		vfd.writePacket(msg, 38, 10000);
+		const uint8_t* msg = (uint8_t*) "\x1B\x00\x25\x1D\x10\x15\x1FPLEASE WAIT\x10\x1E\x29INITIALIZNG SYSTEM";
+		vfd.writePacket(msg, 39, 10000);
 		vfd.writePacket((uint8_t*)"\x00", 1, 10000);
 	
 		sleep(1);
